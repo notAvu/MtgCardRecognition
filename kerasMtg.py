ab4765b9fa225758fcb4ac7f64a764973b73ffc5
Álvaro Fernández <75076925+notAvu@users.noreply.github.com>
2023-06-28 21:05:53 +0200

    Attempt to read full card name

--- kerasMtg.py
+++ kerasMtg.py
@@ -9,28 +9,26 @@ import matplotlib.pyplot as plt
 import keras_ocr
 import numpy as np
 import pandas as pd
-# class mtgHelper:
-#     card
+
+
 class MtgImageRecognition:
     def __init__(self, images):
         self.imgs = images
         self.pipeline = keras_ocr.pipeline.Pipeline()
     def load_card(self):
         self.pred = self.pipeline.recognize(self.imgs)
-        self.textDf = pd.DataFrame(self.pred[0], columns=['text', 'box'])
+        self.textDf_list = []
+        for p in self.pred: 
+            self.textDf_list.append(pd.DataFrame(p, columns=['text', 'box']))
     def show_card(self, index):
         fig, ax = plt.subplots(figsize=(10, 10))
         keras_ocr.tools.drawAnnotations(self.imgs[index], self.pred[index], ax=ax)
         plt.show()
-        # fig, axs = plt.subplots(nrows=len(self.imgs), figsize=(20,20))
-        # for ax, image, predictions in zip(axs, self.imgs, self.pred):
-        #     keras_ocr.tools.drawAnnotations(image=image, predictions=predictions, ax = ax)
-    def get_card_name(self):
-        # self.pipeline.recognize(self.imgs)
-        return 2
+    def get_card_name(self, index): self.pred[index]
 
 img = keras_ocr.tools.read("https://i.ebayimg.com/images/g/ecUAAOSw07diceIX/s-l1600.jpg")
 yuriko = keras_ocr.tools.read("https://i.ebayimg.com/images/g/s~UAAOSws2dixw0m/s-l1200.webp")
 ir = MtgImageRecognition([img,yuriko])
 ir.load_card()
+# print(list(ir.get_card_name(1)))
 ir.show_card(1)
