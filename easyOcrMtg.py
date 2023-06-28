d1d24036829574ef5cca0603b64ba04b1489ec80
Álvaro Fernández <75076925+notAvu@users.noreply.github.com>
2023-06-28 22:55:35 +0200

    Update easyOcrMtg.py

--- easyOcrMtg.py
+++ easyOcrMtg.py
@@ -1,8 +1,4 @@
 from mtgsdk import Card, Set
-# cardName = input("Name of the card: ")
-# card = Card.where(name = cardName).all()
-# for c in card:
-#     print(f"Card name: {c.name} \n Cost {c.mana_cost}")
 import easyocr
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -11,5 +7,9 @@ img= plt.imshow(plt.imread("./Gobrecruiter.jpg"))
 reader = easyocr.Reader(['en', 'es'], gpu= False)
 results = reader.readtext("./Gobrecruiter.jpg")
 rDf=pd.DataFrame(results)
-plt.show()
+# plt.show()
 print(rDf[1][0])
+cardName = rDf[1][0]
+card = Card.where(name = cardName).all()
+for c in card:
+    print(f"Card: {c.name} ({c.set}) \n Cost: {c.mana_cost} \n Cardmarket: {3}€")
